cadb936be3069f0a94adf50e2be7027fd77e25a8
jibidus <jean-baptiste@sogilis.com>
2021-02-03 11:25:19 +0000

    fifteen multiples

--- test.py
+++ test.py
@@ -3,7 +3,9 @@ import unittest
 def fizzbuzz():
     numbers = []
     for i in range(1, 101):
-        if (i % 3 == 0):
+        if (i % 15 == 0):
+            numbers.append("FizzBuzz")
+        elif (i % 3 == 0):
             numbers.append("Fizz")
         elif (i % 5 == 0):
             numbers.append("Buzz")
@@ -23,6 +25,12 @@ class FizzBuzzTest(unittest.TestCase):
   def test_five_multiples_return_Buzz(self):
       self.assertEqual("Buzz", fizzbuzz()[4])
       self.assertEqual("Buzz", fizzbuzz()[9])
+
+  def test_fifteen_multiples_return_FizzBuzz(self):
+      self.assertEqual("FizzBuzz", fizzbuzz()[14])
+      self.assertEqual("FizzBuzz", fizzbuzz()[29])
+
+  
 
 if __name__ == '__main__':
   unittest.main()
